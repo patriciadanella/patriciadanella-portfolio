d85596ffdbfaafed40b11e755f5a0b991680fda2
aethena <patriciadanella.pd@gmail.com>
2022-08-29 22:47:27 +0700

    Updates

---
+++ precache-manifest.7fdf15f26bebd5fa03c6d5df287d1ca5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d67b48b7405609ab6cd9df9a8749baa5",
+    "url": "/patriciadanella-portfolio/index.html"
+  },
+  {
+    "revision": "0f0fad0afd962eef582d",
+    "url": "/patriciadanella-portfolio/static/css/2.a3842014.chunk.css"
+  },
+  {
+    "revision": "0d5307d542e3eaa926ca",
+    "url": "/patriciadanella-portfolio/static/css/main.71fe56fd.chunk.css"
+  },
+  {
+    "revision": "0f0fad0afd962eef582d",
+    "url": "/patriciadanella-portfolio/static/js/2.296d9c86.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/patriciadanella-portfolio/static/js/2.296d9c86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d5307d542e3eaa926ca",
+    "url": "/patriciadanella-portfolio/static/js/main.7ed2ce6c.chunk.js"
+  },
+  {
+    "revision": "76ca5670c850bb451058",
+    "url": "/patriciadanella-portfolio/static/js/runtime-main.17c56e4c.js"
+  }
+]);
